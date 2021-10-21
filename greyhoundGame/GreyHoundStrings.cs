5ef67006ca62cbbb786f69712853cc098328d873
michael oates <moates@gmail.com>
2021-10-21 17:11:20 +1100

    some class formatting, started adding races

---
+++ greyhoundGame/GreyHoundStrings.cs
@@ -0,0 +1,30 @@
+﻿namespace greyhoundGame
+{    public static class GreyHoundStrings
+    {
+        //a list of strings to apply to names and descriptions of statistics
+        public static string TopSpeed = "Top Speed";
+        public static string TopSpeedDesc = "Measures the fastest a greyhound can travel";
+        public static string Stamina = "Stamina";
+        public static string StaminaDesc = "How long can a greyhound run?";
+        public static string Accelertion = "Acceleration";
+        public static string AccelerationDesc = "How quickly the animal will reach top speed";
+        public static string Health = "Health";
+        public static string HealthDesc = "A measure of how much a greyhound can be hurt";
+        public static string Armor = "Armor";
+        public static string ArmorDesc = "The natural protection of the animal";
+        public static string Confidence = "Confidence";
+        public static string ConfidenceDesc = "How confident is the animal, how likely is it to take risks?";
+        public static string Tenacity = "Tenacity";
+        public static string TenacityDesc = "How long can the greyhound keep this up?";
+        public static string Balance = "Balance";
+        public static string BalanceDesc = "Whether or not the animal can keep its feet";
+        public static string Agility = "Agility";
+        public static string AgilityDesc = "The greyhound's ability to move effectively... when it needs to";
+        public static string Strength = "Strength";
+        public static string StrengthDesc = "Will the animal come out on top?";
+        public static string Aggression = "Aggression";
+        public static string AggressionDesc = "Will this greyhound start a fight?";
+        public static string Fitness = "Fitness";
+        public static string FitnessDesc = "How appropriate is this animal for its environment?";
+    }
+}
--- greyhoundGame/Program.cs
+++ greyhoundGame/Program.cs
@@ -1,51 +1,83 @@
-﻿namespace greyhoundGame
+﻿using System;
+
+namespace greyhoundGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Let's build us a baby greyhound!");
+            Console.WriteLine("Building dat greyhound!");
+            Greyhound baby = new Greyhound();
+            Console.WriteLine(baby.ToString());   
+
         }
     }
 
-    public static class GreyHoundStrings
+    #region TODO
+    public class Mutator
     {
-        //a list of strings to apply to names and descriptions
-        public static string TopSpeed = "Top Speed";
-        public static string TopSpeedDesc = "Measures the fastest a greyhound can travel";
-        public static string Stamina = "Stamina";
-        public static string StaminaDesc = "How long can a greyhound run?";
-        public static string Accelertion = "Acceleration";
-        public static string AccelerationDesc = "How quickly the animal will reach top speed";
-        public static string Health = "Health";
-        public static string HealthDesc = "A measure of how much a greyhound can be hurt";
-        public static string Armor = "Armor";
-        public static string ArmorDesc = "The natural protection of the animal";
-        public static string Confidence = "Confidence";
-        public static string ConfidenceDesc = "How confident is the animal, how likely is it to take risks?";
-        public static string Tenacity = "Tenacity";
-        public static string TenacityDesc = "How long can the greyhound keep this up?";
-        public static string Balance = "Balance";
-        public static string BalanceDesc = "Whether or not the animal can keep its feet";
-        public static string Agility = "Agility";
-        public static string AgilityDesc = "The greyhound's ability to move effectively... when it needs to";
-        public static string Strength = "Strength";
-        public static string StrengthDesc = "Will the animal come out on top?";
-        public static string Aggression = "Aggression";
-        public static string AggressionDesc = "Will this greyhound start a fight?";
-        public static string Fitness = "Fitness";
-        public static string FitnessDesc = "How appropriate is this animal for its environment?";
+        // everything that can affect a greyhound needs to be a mutator
     }
+
+    public class Personality : Mutator
+    {
+        // a greyhound's personality is a mutator
+
+    }
+
+    public class Daughter : Mutator
+    {
+        // a daughter is a thing that a greyhound can be given to change it
+    }
+
+    public class Injury : Mutator
+    {
+        // a greyhound can be injured
+    }
+
+    public class Mood : Mutator
+    {
+        // a greyhound can have a mood
+    }
+
+    public class Weight : Mutator
+    {
+        // a greyhound has a weight
+    }
+    #endregion
 
     class Greyhound
     {
         // a greyhound has statistics
         public StatList Stats { get; set; }
+        // a greyhound has a name
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        public Greyhound()
+        {
+            // a baby greyhound
+            Stats = new StatList();
+            Name = "Baby Greyhound";
+            Age = 0;
+        }
 
+        public override string ToString()
+        {
+            string outString = "";
+
+            outString += $"Name: {Name}\n";
+            outString += $"Age: {Age}\n";
+            outString += Stats.ToString();
+
+            return outString;
+        }
     }
 
     class Stat
     {
-        // the value of the statistic can't be less than 1, can't be more than 120
+        // the value of the statistic can't be less than 1, can't be more than 120, statistics over 80 are exceptional, over 100 unique
         private int _statistic;
         public int StatValue
         {
@@ -67,6 +99,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
 
+        // constructors. there's no point of having a greyhound without a stat, also gamefalldown so have a stat please!
         public Stat()
         {
             StatValue = 1;
@@ -77,6 +110,7 @@
             StatValue = stat;
         }
 
+        // we want to give a greyhound basic stats starting with 1 
         public Stat(string name, string desc)
         {
             Name = name;
@@ -84,16 +118,24 @@
             StatValue = 1;
         }
 
+        // giving a statistic a name, a description and a number
         public Stat(string name, string desc, int newStat)
         {
             StatValue = newStat;
             Name = name;
             Description = desc;
         }
+
+        public override string ToString()
+        {
+            return $"{Name}: {StatValue} - {Description}\n";
+        }
     }
 
     class StatList
     {
+        // this is a list of statistics that all greyhounds have, valued from 1 to 120
+
         public Stat TopSpeed { get; set; }
         public Stat Stamina { get; set; }
         public Stat Acceleration { get; set; }
@@ -102,7 +144,7 @@
         public Stat Confidence { get; set; }
         public Stat Tenacity { get; set; }
         public Stat Balance { get; set; }
-        public Stat Agilty { get; set; }
+        public Stat Agility { get; set; }
         public Stat Strength { get; set; }
         public Stat Aggression { get; set; }
         public Stat Fitness { get; set; }
@@ -130,12 +172,13 @@
                 Confidence,
                 Tenacity,
                 Balance,
-                Agilty,
+                Agility,
                 Strength,
                 Aggression,
                 Fitness
             };
 
+            Console.WriteLine(returnList.Length);
             return returnList;
         }
 
@@ -150,6 +193,7 @@
             Confidence = new Stat(GreyHoundStrings.Confidence, GreyHoundStrings.ConfidenceDesc);
             Tenacity = new Stat(GreyHoundStrings.Tenacity, GreyHoundStrings.TenacityDesc);
             Balance = new Stat(GreyHoundStrings.Balance, GreyHoundStrings.BalanceDesc);
+            Agility = new Stat(GreyHoundStrings.Agility, GreyHoundStrings.AgilityDesc);
             Strength = new Stat(GreyHoundStrings.Strength, GreyHoundStrings.StrengthDesc);
             Aggression = new Stat(GreyHoundStrings.Aggression, GreyHoundStrings.AggressionDesc);
             Fitness = new Stat(GreyHoundStrings.Fitness, GreyHoundStrings.FitnessDesc);
@@ -163,7 +207,113 @@
 
             // assigning stats
             for (int i = 0; i < list.Length; i++)
+            {
+                //Console.WriteLine(i);
                 list[i].StatValue = stats[i];
+            }
+        }
+
+        public override string ToString()
+        {
+            string outString = "";
+            // get the ToStrings() from the stats
+            Stat[] stats = ListStats();
+
+            foreach (Stat stat in stats)
+            {
+                outString += stat.ToString();
+            }
+            return outString;
         }
     }
+
+    class Race
+    {
+        // greyhounds run a race, lets see who's the fastest!
+        // the greyhounds in the race
+        public Greyhound[] Greyhounds { get; private set; }
+        
+        //the length of the race
+        public int Length { get; private set; }
+
+        private RaceGreyhound[] raceHounds;
+
+        public Race(Greyhound[] greyhounds)
+        {
+            Greyhounds = greyhounds;
+            Length = 500;
+            raceHounds = new RaceGreyhound[greyhounds.Length];
+
+            // so we're putting them all in the race, registering them basically and salting them at the same time
+            for (int i = 0; i < Greyhounds.Length; i++)
+            {
+                raceHounds[i] = new RaceGreyhound(Greyhounds[i]);
+            }
+        }
+
+        public Race(Greyhound[] greyhounds, int length )
+        {
+            Greyhounds = greyhounds;
+            Length = length;
+        }
+
+        public Results Start()
+        {
+            
+        }
+
+
+    }
+
+    class RaceGreyhound
+    {
+        // we're using this class to describe a greyhound on raceday so we're not altering its stats on the day
+        // I guess we're using this to add a bunch of data to greyhound
+        Greyhound Hound;
+        public int CurrentSpeed { get; set; }
+        public int CurrentStam { get; set; }
+        public int SaltedTopSpeed { get; private set; }
+        public int SaltedTenacity { get; private set; }
+        public int SaltedAcceleration { get; private set; }
+
+        public RaceGreyhound(Greyhound hound)
+        {
+            Hound = hound;
+            CurrentSpeed = 0;
+            CurrentStam = Hound.Stats.Stamina.StatValue + getSalt();
+            SaltedTopSpeed = Hound.Stats.TopSpeed.StatValue + getSalt();
+            SaltedTenacity = Hound.Stats.Tenacity.StatValue + getSalt();
+            SaltedAcceleration = Hound.Stats.Tenacity.StatValue + getSalt();
+        }
+
+        // generating a modifier to make thing random
+        private int getSalt()
+        {
+            Random dice = new Random();
+
+            int result = dice.Next(1, 100);
+
+            // so each statment has a return on it so I'm eliminating results
+            // I'm not sure if the result periods are even and I don't care right now
+            if (result <= 4)
+                return -20; //nasty!
+            if (result <= 20)
+                return -10;
+            if (result <= 40)
+                return -5;
+            if (result <= 60)
+                return 0;
+            if (result <= 80)
+                return 5;
+            if (result <= 97)
+                return 10;
+            return 20; // aaaaawesome!
+        }
+    }
+
+    class Results
+    {
+        // todo, class to hold results
+    }
+
 }
